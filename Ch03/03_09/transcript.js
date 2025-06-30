5bbe784e3af9c9ec57f57daf727670a24f774ecb
Pranavi-1125 <mpranavi1125@gmail.com>
2025-06-30 06:51:02 +0530

    Update transcript.js

--- Ch03/03_09/transcript.js
+++ Ch03/03_09/transcript.js
@@ -3,22 +3,22 @@ var string2 = "This is the shortest string ever.";
 var string3 = "Is this the thing called Mount Everest?";
 var string4 = "This is the Sherman on the Mount.";
 
-var regex = /this/;
+var regex = /this/; // It is case sensitive "this" is not same as "This"
 
 regex.test(string1);
 regex.test(string2);
 regex.test(string3);
 regex.test(string4);
 
-regex = /this/i;
+regex = /this/i; // Removes case sensitive "this" is same as "This"
 
-regex = /^this/i;
+regex = /^this/i; // Starting with "this"
 
-regex = /this$/i;
+regex = /this$/i;// Ending with "this"
 
-regex = /ever.$/i;
+regex = /ever.$/i; //Ending with "ever"
 
-regex = /ever\.$/i;
+regex = /ever\.$/i; // Ending with "ever."
 
 // More info:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
