41f8ce64bd8acc7156f602a4142f53052a462c38
Pranavi-1125 <mpranavi1125@gmail.com>
2025-07-10 18:13:42 +0530

    Update transcript.js

--- Ch04/04_07/transcript.js
+++ Ch04/04_07/transcript.js
@@ -5,6 +5,8 @@ animal === "cat"
   ? console.log("You will be a cat herder.")
   : console.log("You will be a dog catcher.");
 
+// animal === "cat"? console.log("You will be a cat herder.") : console.log("You will be a dog catcher.");
+
 var job = animal === "cat" ? "cat herder" : "dog catcher";
 
 // prettier-ignore
