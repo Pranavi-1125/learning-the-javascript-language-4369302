266aede88dbed4f6be46b268b447e51b93499196
Pranavi-1125 <mpranavi1125@gmail.com>
2025-07-10 17:26:13 +0530

    Update transcript.js
    Added few comments

--- Ch04/04_01/transcript.js
+++ Ch04/04_01/transcript.js
@@ -7,9 +7,9 @@ one !== two; // true
 one === two; // false
 
 one == one; // true
-one == "1"; // true (?!)
+one == "1"; // true (?!) // By default When 1 is assigned to one, 1 is changed to '1'(string). So it return true when we compare 1 = '1'
 one != "1"; // false (?!)
-one === "1"; // false
+one === "1"; // false // Here it checks the datatye so it is false
 
 one < two; // true
 
