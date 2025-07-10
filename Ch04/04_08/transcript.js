04ebd6f2f9a933941f08035a25252238075c4095
Pranavi-1125 <mpranavi1125@gmail.com>
2025-07-10 18:27:27 +0530

    Update transcript.js

--- Ch04/04_08/transcript.js
+++ Ch04/04_08/transcript.js
@@ -1,35 +1,36 @@
 var thing = 12;
 thing = "twelve";
-typeof thing;
+typeof thing; //String
 
 thing = 12;
-typeof thing;
+typeof thing; //Number
 
 thing = false;
-typeof thing;
+typeof thing; //Boolean
 
 thing = {};
-typeof thing;
+typeof thing; //Object
 
-thing = [];
-typeof thing;
+thing = []; //Object
+typeof thing; // Here for we dont know if it is a array because it gives object as its type
 typeof thing === "object" && thing.hasOwnProperty("length"); // true
+// So we use hasOwnProperty() - this checks if there is a property of that object called "length" and we can confirm that it is an array
 
 thing = {};
-typeof thing === "object" && thing.hasOwnProperty("length"); // false
+typeof thing === "object" && thing.hasOwnProperty("length"); // false // Not an array
 
-NaN;
-typeof NaN;
-Number.isNaN();
+NaN; //Not A Number
+typeof NaN; //Number
+Number.isNaN(); //to check whether the var is NaN the this is the way Number.isNaN(NaN)
 
-typeof null;
-thing === null;
+typeof null; // Object
+thing === null; // These are the ways to check if it is null
 thing = null;
 thing === null;
 
-let somethingLater;
-typeof somethingLater;
-typeof nothingSilly;
+let somethingLater; // Undefined
+typeof somethingLater;  // Undefined
+typeof nothingSilly; // Undefined
 
 // More info:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
